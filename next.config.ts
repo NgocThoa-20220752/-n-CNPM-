08981f591a1c355ca094c76b1e3209ac0b1eb7b3
NgocThoa-20220752 <20220752@eaut.edu.vn>
2025-11-13 10:13:43 +0700

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // appDir đã được tích hợp sẵn trong Next.js 15, không cần khai báo experimental
-  // Các cấu hình khác nếu có
-};
-
-export default nextConfig;
